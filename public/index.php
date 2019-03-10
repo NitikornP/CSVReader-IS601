dca85d618132335f56d508db681e32ee3d5d90fe
nitikorn <np638@njit.edu>
2019-03-09 22:30:02 -0500

    Display a table

--- public/index.php
+++ public/index.php
@@ -44,19 +44,18 @@ class html {
             $count++;
         }
 
-        echo "<html><body><table>\n\n";
-        $f = fopen("example.csv", "r");
-        while (($line = fgetcsv($f)) !== false) {
-            echo "<tr>";
-            foreach ($line as $cell) {
-                echo "<td>" . htmlspecialchars($cell) . "</td>";
-            }
-            echo "</tr>\n";
-        }
-        fclose($f);
-        echo "\n</table></body></html>";
-
-
+                 echo "<html><body><table>\n\n";
+                 $f = fopen("example.csv", "r");
+                 while (($line = fgetcsv($f)) !== false) {$row = $line[0];
+                 $cells = explode(";",$row);
+                 echo "<tr>";
+                 foreach ($cells as $cell) {
+                      echo "<td>" . htmlspecialchars($cell) . "</td>";
+                        }
+                      echo "</tr>\n";
+                    }
+                   fclose($f);
+                  echo "\n</table></body></html>";
     }
 }
 class csv {
