52dd5395a1bdb114f6ba58e209216221da00d4f7
nitikorn <np638@njit.edu>
2019-03-09 22:26:21 -0500

    Display a table

--- public/index.php
+++ public/index.php
@@ -8,18 +8,6 @@
 
 main::start("example.csv");
 
-echo "<html><body><table>\n\n";
-$f = fopen("example.csv", "r");
-while (($line = fgetcsv($f)) !== false) {
-    echo "<tr>";
-    foreach ($line as $cell) {
-        echo "<td>" . htmlspecialchars($cell) . "</td>";
-    }
-    echo "</tr>\n";
-}
-fclose($f);
-echo "\n</table></body></html>";
-
 class main {
 
     static public function start($filename) {
@@ -55,6 +43,18 @@ class html {
             }
             $count++;
         }
+
+        echo "<html><body><table>\n\n";
+        $f = fopen("example.csv", "r");
+        while (($line = fgetcsv($f)) !== false) {
+            echo "<tr>";
+            foreach ($line as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+            }
+            echo "</tr>\n";
+        }
+        fclose($f);
+        echo "\n</table></body></html>";
 
 
     }
